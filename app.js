3a7493a82318e574c8693cc43caa940d9b3884ef
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-04-03 11:20:32 -0600

    Add babel and create app.js file

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const shortid = require('shortid');
+app.use(express.json());
+app.use(cors());
+
+app.locals.notes = [
+  { id: 12345, name: 'worf', type: 'cat' }
+]
+
+app.get('/api/v1/notes', (request, response) => {
+  response.status(200).json(app.locals.notes);
+});
+
+app.post('/api/v1/notes', (request, response) => {
+  if (!request.body.type && !request.body.name) {
+    return response.status(422).send({
+      success: 'false',
+      message: 'name & type is required'
+    });
+  } else if (!request.body.name) {
+    return response.status(422).send({
+      success: 'false',
+      message: 'name is required'
+    });
+  } else if (!request.body.type) {
+    return response.status(422).send({
+      success: 'false',
+      message: 'type is required'
+    });
+  } else if (request.body.type && request.body.name) {
+    const note = {
+      id: shortid.generate(),
+      name: request.body.name,
+      type: request.body.type
+    }
+    app.locals.notes.push(note);
+    return response.status(201).json(note);
+  }
+});
+
+app.get('/api/v1/notes/:id', (request, response) => {
+  const noteById = app.locals.notes.find(note => request.params.id == note.id)
+  if(!noteById) return response.sendStatus(404)
+  response.status(200).json(noteById)
+})
+
+export default app
