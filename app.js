c382e1cce85ad08108fa208023d0b17d8b95700c
Michael Krog <michaelryankrog@gmail.com>
2019-04-25 12:27:21 -0600

    updates for annotation

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const express = require('express');
-const app = express();
-const cors = require('cors')
-const shortid = require('shortid');
-app.use(express.json());
-app.use(cors());
+const express = require('express'); // this brings in express.js and everything with it to use in the node.js server file
+const app = express(); // app is the instance and creates an Express application
+const cors = require('cors') // brings in Cross Origin Resource Sharing  and assigns to variable cors
+const shortid = require('shortid'); // brings in shortid to generate random numbers for creating ids and assigns to shortid
+app.use(express.json()); // tells express to parses incoming requests with JSON payloads and is based on body-parser.
+app.use(cors()); // tells express / server to user Cross Origin Resource Sharing variable above
 
+// This is telling express to create local a variable
+// The app.locals object has properties that are local variables within the application and is assigned to notes
+// This is just storing a bunch of pre generated objects in the varaible app.locals.notes
+// Once set, the value of app.locals properties persist throughout the life of the application
 app.locals.notes = [
   { id: shortid.generate(),
     title: 'Evaluation To-Do',
@@ -250,36 +254,49 @@ app.locals.notes = [
   },
 ];
 
+// this is a sendMessage function that is a reusable piece of code to send status' of requests made below
+// repsonse is the response object being sent by the server, code is the number assigned by the type of status
+// message is either the object being passed back through the response or a string to give more information on the status code
 const sendMessage = (response, code, message) => {
  return response.status(code).json(message)
 }
 
+// client endpoint to get app.locals.notes express variable
+// sends the response code of 200 = ok and all the notes stored in the variable
 app.get('/api/v1/notes', (request, response) => {
  sendMessage(response, 200, app.locals.notes);
 });
 
 app.post('/api/v1/notes', (request, response) => {
-  const { notes } = app.locals;
-  const { title, list, background } = request.body;
+  const { notes } = app.locals; // destructing app.locals.notes to be able to just use variable notes
+  const { title, list, background } = request.body; // destructing title, list, background request information from request.body
 
-  if (!title) return sendMessage(response, 422, 'Title is required');
+  // 422 = unprosscable entry
+  if (!title) return sendMessage(response, 422, 'Title is required'); // returns a response with 422 code and string message that user did not include title
 
+  // generates a note with the clients request of background, title, list, as well generates a unique id
   const note = {
     id: shortid.generate(),
     background,
     title,
     list
   }
-  notes.push(note);
-  return response.status(201).json(note);
-  });
+  notes.push(note); // pushes the note into the app.locals.notes express variable
+  return response.status(201).json(note); // returns a response with 201 code and the new note created '201 = created'
+});
 
 app.put('/api/v1/notes/:id', (request, response) => {
-  const { title, list, background } = request.body;
-  const { id } = request.params;
-  const { notes } = app.locals;
+  const { title, list, background } = request.body; // destructing title, list, background request information from request.body
+  const { id } = request.params; // destructing id from the url being sent from the clients request
+  const { notes } = app.locals; // destructing app.locals.notes to be able to just use variable notes
 
-  let found = false;
+  let found = false; // creates varaible found and assigns bool false
+
+  // creats variable newNotes
+  // maps over app.localas.notes and finds the note id that matches the id sent in the client requests
+  // when it matches the id stays the same but will then look and see if there is a background, title, or list
+  // request.body property it will overwrite the original. if not it will assign it the previous one
+  // after done mapping over notes it will assign all the notes to newNotes varialbe
   const newNotes = notes.map(note => {
     if (note.id == id) {
       found = true;
@@ -293,24 +310,28 @@ app.put('/api/v1/notes/:id', (request, response) => {
       return note
     }
   });
-  if(!found) return sendMessage(response, 404, 'Note was not found');
-  if(!title) return sendMessage(response, 400, 'Title is required');
-  app.locals.notes = newNotes;
+  if(!found) return sendMessage(response, 404, 'Note was not found'); // returns a response with 404 code and string message of note not found
+  if(!title) return sendMessage(response, 400, 'Title is required'); // returns a response with 400 code and string message that user did not include title
+  app.locals.notes = newNotes; // reassigning app.locals.notes to the newNotes variable created with the new note stored
 
-  return sendMessage(response, 200, 'Note updated successfully');
+  return sendMessage(response, 200, 'Note updated successfully'); // returns a response with 200 code and string message
 });
 
+// this client endpoint will find a specific note and return that note in the response
 app.get('/api/v1/notes/:id', (request, response) => {
- const noteById = app.locals.notes.find(note => request.params.id == note.id);
- if(!noteById) return sendMessage(response, 404, 'Note was not found');
- sendMessage(response, 200, noteById);
+ const noteById = app.locals.notes.find(note => request.params.id == note.id); // finds the app.locals.notes specific note by id by matching the id by the one sent in the request
+ if(!noteById) return sendMessage(response, 404, 'Note was not found'); // returns a response with 404 code and string message of note not found
+ sendMessage(response, 200, noteById); // invokes the sendMessage function and sends the response, 200 status code of ok, and then the specific note found
 })
 
+// this client endpoint will find the specific note and delete the note from the app.locals.notes variable
 app.delete('/api/v1/notes/:id', (request, response) => {
-  const noteIndex = app.locals.notes.findIndex(note => note.id == request.params.id);
-  if (noteIndex === -1) return response.status(404).json('Note not found');
-  app.locals.notes.splice(noteIndex, 1);
-  return sendMessage(response, 200, 'Note was successfully deleted');
+  const noteIndex = app.locals.notes.findIndex(note => note.id == request.params.id); // finds the index of the note id matching the request id in the app.locals.notes varialbe
+  if (noteIndex === -1) return response.status(404).json('Note not found'); // returns a response with 404 code and string message of note not found
+  app.locals.notes.splice(noteIndex, 1); // splices out the index found from the app.locals.notes variable
+  return sendMessage(response, 200, 'Note was successfully deleted'); // returns a response with 200 code and string message
 })
 
+
+// exports the express instance of app to be used by other files
 export default app;
